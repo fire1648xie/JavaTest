04cebffccf7d56e27c0bb9c44585d213c18dded5
Oli <fire1648xie@gmail.com>
2017-06-19 03:34:07 +0800

    Test function

--- src/Hello.java
+++ src/Hello.java
@@ -2,10 +2,15 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 /**
- * Created by Oli on 2017/6/18.
+ * Created by Oli on 2017/6/18.for Test1
  */
 public class Hello {
     public static void main(String args[])
+    {
+        Hello h = new Hello();
+        h.test();
+    }
+    private void test()
     {
         System.out.println("Hello there, im back");
         Calendar cal = Calendar.getInstance();
@@ -13,5 +18,4 @@ public class Hello {
         System.out.println(sdf.format(cal.getTime()));
         System.out.println("What should I do?");
     }
-
 }
